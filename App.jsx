9ef8946a7a2b985d731f945b20469eeee1a6e769
Gabriel <gabrielsb1090@gmail.com>
2026-05-22 08:31:38 -0300

    feat: Filtrando paises com base no valor digitado no input de pesquisa

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [countries, setCountries] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [valor, setValor] = useState("")
 
   useEffect(() => {
     async function fetchCountries() {
@@ -25,8 +26,19 @@ function App() {
   if (loading) return <p>Carregando...</p>
   if (error) return <p>Erro: {error}</p>
 
+  const paisesFiltrados = countries.filter((pais) =>
+    pais.name.common.toLowerCase().includes(valor.toLowerCase())
+  )
+
   return (
-    <Pesquisa countries={countries}/>
+    <>
+      <Pesquisa valor={valor} setValor={setValor} />
+      <ul>
+        {paisesFiltrados.map((pais) => (
+          <li key={pais.name.common}>{pais.name.common}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
