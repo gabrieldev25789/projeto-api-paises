c38f1c8c7f3e599e39efc66420445d33b8b21106
Gabriel <gabrielsb1090@gmail.com>
2026-05-22 05:11:24 -0300

    Criando novo arquivo

---
+++ Components/Pesquisa.jsx
@@ -0,0 +1,29 @@
+function Pesquisa({countries}) {
+
+    countries.forEach((pais)=>{
+        const nome = pais.name.common
+        console.log(nome)
+    })
+
+  return (
+    <section className="pesquisa">
+      <div className="pesquisa-container">
+        <label htmlFor="input-pais">
+          Pesquisar país
+        </label>
+
+        <input
+          id="input-pais"
+          type="text"
+          placeholder="Digite o nome do país..."
+        />
+
+        <button>
+          Pesquisar
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default Pesquisa
