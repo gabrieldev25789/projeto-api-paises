da51c48d253dc97ba1d0e295e8e2366a092c7804
Gabriel <gabrielsb1090@gmail.com>
2026-05-23 03:56:26 -0300

    feat: Adicionando filtros

--- Components/Filtros.jsx
+++ Components/Filtros.jsx
@@ -1,7 +1,43 @@
 import "./Filtros.css"
 
-function Filtros() {
-    console.log("AAAAAAAAAAAAAAA")
+function Filtros({countries, setAfricaFilter, setAmericaFilter, setAsiaFilter, setEuropeFilter, setOceaniaFilter, setAntarcticFilter}) {
+
+    const africa = () => {
+        const resultado = countries.filter((pais) => pais.region === "Africa")
+        console.log(resultado)
+        setAfricaFilter(true)
+    }
+
+    const america = () => {
+        const resultado = countries.filter((pais) => pais.region === "Americas")
+        console.log(resultado)
+        setAmericaFilter(true)
+    }
+
+    const asia = () => {
+        const resultado = countries.filter((pais) => pais.region === "Asia")
+        console.log(resultado)
+        setAsiaFilter(true)
+    }
+
+    const europe = () => {
+        const resultado = countries.filter((pais) => pais.region === "Europe")
+        console.log(resultado)
+        setEuropeFilter(true)
+    }
+
+    const oceania = () => {
+        const resultado = countries.filter((pais) => pais.region === "Oceania")
+        console.log(resultado)
+        setOceaniaFilter(true)
+    }
+
+    const antarctic = () => {
+        const resultado = countries.filter((pais) => pais.region === "Antarctic")
+        console.log(resultado)
+        setAntarcticFilter(true)
+    }
+
   return (
     <div className="filters">
 
@@ -9,12 +45,12 @@ function Filtros() {
         <p className="filter-label">Região</p>
         <div className="region-buttons">
           <button className="region-btn active" data-region="">Todas</button>
-          <button className="region-btn" data-region="Africa">África</button>
-          <button className="region-btn" data-region="Americas">Américas</button>
-          <button className="region-btn" data-region="Asia">Ásia</button>
-          <button className="region-btn" data-region="Europe">Europa</button>
-          <button className="region-btn" data-region="Oceania">Oceania</button>
-          <button className="region-btn" data-region="Antarctic">Antártica</button>
+          <button className="region-btn" onClick={() => africa()} data-region="Africa">África</button>
+          <button className="region-btn" onClick={() => america()} data-region="Americas">Américas</button>
+          <button className="region-btn" onClick={() => asia()} data-region="Asia">Ásia</button>
+          <button className="region-btn" onClick={() => europe()} data-region="Europe">Europa</button>
+          <button className="region-btn" onClick={() => oceania()} data-region="Oceania">Oceania</button>
+          <button className="region-btn" onClick={() => antarctic()} data-region="Antarctic">Antártica</button>
         </div>
       </div>
 
