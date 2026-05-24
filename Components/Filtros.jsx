623431fd9d89dc274f5acfd4c33c82c6a46c4d2e
Gabriel <gabrielsb1090@gmail.com>
2026-05-24 05:32:04 -0300

    refactor: Refatorando funções de filtragem

--- Components/Filtros.jsx
+++ Components/Filtros.jsx
@@ -1,54 +1,17 @@
 import { useState } from "react"
 import "./Filtros.css"
 
-function Filtros({ setRegiao, setMaiorUmMi, setMaiorDezMi, setMaiorCemMi, setMenorUmMi, setQualquerTamanho }) {
+function Filtros({ setRegiao, setPopulacao }) {
 
     const [active, setActive] = useState("")
 
-    function escolherPais(valor) {
-      setActive(valor)
-      setRegiao(valor)
-    }
+  function escolherPais(valor) {
+    setActive(valor)
+    setRegiao(valor)
+  }
 
-    const mais1Mi = () => {
-      setMaiorUmMi(true)
-      setMaiorDezMi(false)
-      setMaiorCemMi(false)
-      setMenorUmMi(false)
-      setQualquerTamanho(false)
-    }
-
-    const maisDezMi = () => {
-      setMaiorDezMi(true)
-      setMaiorUmMi(false)
-      setMaiorCemMi(false)
-      setMenorUmMi(false)
-      setQualquerTamanho(false)
-    }
-
-    const maisCemMi = () => {
-      setMaiorCemMi(true)
-      setMaiorDezMi(false)
-      setMaiorUmMi(false)
-      setMenorUmMi(false)
-      setQualquerTamanho(false)
-    }
-
-    const menorUmMi = () => {
-      setMenorUmMi(true)
-      setMaiorCemMi(false)
-      setMaiorDezMi(false)
-      setMaiorUmMi(false)
-      setQualquerTamanho(false)
-    }
-
-    const qualquer = () => {
-      setQualquerTamanho(true)
-      setMenorUmMi(false)
-      setMaiorCemMi(false)
-      setMaiorDezMi(false)
-      setMaiorUmMi(false)
-    }
+  const filtrarPorPop = (valor) => setPopulacao(valor)
+     
 
   return (
     <div className="filters">
@@ -72,14 +35,14 @@ function Filtros({ setRegiao, setMaiorUmMi, setMaiorDezMi, setMaiorCemMi, setMen
         <p className="filter-label">População</p>
         <div className="pop-options">
           <label><input type="radio" name="population" value="" onClick={() => 
-          qualquer()} defaultChecked /> Qualquer tamanho</label>
+          filtrarPorPop("")} defaultChecked /> Qualquer tamanho</label>
           <label><input type="radio" name="population" value="lt1m" onClick={() => 
-          menorUmMi()} /> Menos de 1 milhão</label>
+          filtrarPorPop("lt1m")} /> Menos de 1 milhão</label>
           <label><input type="radio" name="population" value="1m-10m" onClick={() => 
-          mais1Mi()}/> 1 – 10 milhões</label>
-          <label><input type="radio" name="population" value="10m-100m" onClick={() => maisDezMi()}/> 10 – 100 milhões</label>
+          filtrarPorPop("1m-10m")}/> 1 – 10 milhões</label>
+          <label><input type="radio" name="population" value="10m-100m" onClick={() => filtrarPorPop("10m-100m")}/> 10 – 100 milhões</label>
           <label><input type="radio" name="population" value="gt100m" onClick={() => 
-          maisCemMi()} /> Mais de 100 milhões</label>
+          filtrarPorPop("gt100m")} /> Mais de 100 milhões</label>
         </div>
       </div>
 
