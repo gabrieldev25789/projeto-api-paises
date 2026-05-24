a3bcf18879f19243dc1a6925408c2b1ae9ba228d
Gabriel <gabrielsb1090@gmail.com>
2026-05-24 05:10:04 -0300

    feat: Criando estados, filtrando e passando props

--- App.jsx
+++ App.jsx
@@ -11,6 +11,12 @@ function App() {
   const [valor, setValor] = useState("")
   const [regiao, setRegiao] = useState("")
 
+  const [maiorUmMi, setMaiorUmMi] = useState(false)
+  const [maiorDezMi, setMaiorDezMi] = useState(false)
+  const [maiorCemMi, setMaiorCemMi] = useState(false)
+  const [menorUmMi, setMenorUmMi] = useState(false)
+  const [qualquerTamanho, setQualquerTamanho] = useState(false)
+
   useEffect(() => {
     async function fetchCountries() {
       try {
@@ -29,15 +35,29 @@ function App() {
   if (loading) return <p>Carregando...</p>
   if (error) return <p>Erro: {error}</p>
 
-  const paisesFiltrados = countries.filter((pais) =>
-    pais.region.includes(regiao) &&
-    pais.name.common.toLowerCase().includes(valor.toLowerCase())
-  )
+      const paisesFiltrados = countries.filter((pais) => {
+      const porRegiao = pais.region.includes(regiao)
+      const porNome = pais.name.common.toLowerCase().includes(valor.toLowerCase())
+      const porPopQualquer = !qualquerTamanho || (pais.population)
+      const porPopMaiorUmMi = !maiorUmMi || (pais.population >= 1_000_000 && pais.population < 10_000_000)
+      const porPopMaiorDezMi = !maiorDezMi || (pais.population >= 10_000_000 && pais.population < 100_000_000)
+      const porPopMaiorCemMi = !maiorCemMi || (pais.population > 100_000_000)
+      const popMenorUmMi = !menorUmMi || (pais.population < 1_000_000)
+
+      return porRegiao && porNome && porPopMaiorUmMi && porPopMaiorDezMi && porPopMaiorCemMi && popMenorUmMi && porPopQualquer
+    })
 
   return (
     <>
       <Pesquisa valor={valor} setValor={setValor} />
-      <Filtros setRegiao={setRegiao} />
+      <Filtros 
+      setRegiao={setRegiao} 
+      countries={countries} 
+      setMaiorUmMi={setMaiorUmMi}
+      setMaiorDezMi={setMaiorDezMi}
+      setMaiorCemMi={setMaiorCemMi}
+      setMenorUmMi={setMenorUmMi}
+      setQualquerTamanho={setQualquerTamanho}/>
 
     <div className="cards-grid">
       {paisesFiltrados.map((pais) => (
